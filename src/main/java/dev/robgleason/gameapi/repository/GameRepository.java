cf2dbef38ac3ae8a79bb469f381c3fdbb780f988
Robert Gleason <l2ob122@gmail.com>
2023-08-22 17:07:50 -0700

    messing around with application propeties

--- src/main/java/dev/robgleason/gameapi/repository/GameRepository.java
+++ src/main/java/dev/robgleason/gameapi/repository/GameRepository.java
@@ -3,6 +3,8 @@ package dev.robgleason.gameapi.repository;
 import dev.robgleason.gameapi.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+//@RepositoryRestResource(path = "videogames")
 public interface GameRepository extends JpaRepository<Game, Integer> {
-    
+
 }
